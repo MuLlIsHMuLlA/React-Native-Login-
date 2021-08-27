c81c222429c0cff3469f2c786da3adc4c3e54ea6
Mike Kwame <mullishmulla@gmail.com>
2021-08-27 16:03:44 -0700

    react-native (login)

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.logStyle}> Log in</Text>
+      <View>
+        <TextInput style={styles.inputSet} placeholder="Username" />
+        <TextInput style={styles.inputSet} placeholder="Password" />
+      </View>
+
+      <Text style={styles.passForg} >Forgot password?</Text>
+
+      <View style={styles.buttonBox}>
+        <Text style={styles.buttonText}>Log in</Text>
+      </View>
+
+      <View style={styles.noAcc}>
+      <Text>Don't have an account?</Text>
+      <Text style={styles.signUp}>Sign up</Text>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +29,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 60
+
   },
+  logStyle: {
+    color: "#813e9f",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: 80,
+    marginBottom: 80,
+  },
+  inputSet: {
+    borderBottomWidth: 2,
+    borderBottomColor: "#813e9f",
+    marginVertical: 20
+  },
+  passForg: {
+    textAlign: "right",
+    color: "blue",
+    marginBottom: 80
+  },
+  buttonBox: {
+    width: 260,
+    height: 60,
+    borderRadius:12,
+    padding: 10,
+    alignSelf:"center",
+    backgroundColor: "#813e9f",
+  },
+  buttonText: {
+    color: "#ffff",
+    textAlign: "center",
+    padding: 10,
+    fontSize: 20
+  },
+  noAcc: {
+    marginTop: 50,
+    alignItems: "center",
+    flexDirection: "row",
+    marginHorizontal: 20,
+  },
+  signUp: {
+    marginLeft: 10,
+    color: "#813e9f"
+  }
+
 });
